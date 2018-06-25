1f5031876f2c7a3ec99a034ba47601935837e505
Kakeru-Ito <a5816011@aoyama.jp>
2018-06-25 16:22:57 +0900

    2080625commit.fs

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r%n_wfpspvkmi)c^3n058z19hw6n$rjz%pawi_04-z#%)2(nj-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
